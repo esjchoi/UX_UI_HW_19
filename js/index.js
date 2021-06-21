4be768a7b09be3d7f75c86cda8bf77cad70c2a29
esjchoi <77758478+esjchoi@users.noreply.github.com>
2021-06-21 12:32:19 -0700

    changes

--- js/index.js
+++ js/index.js
@@ -17,7 +17,29 @@ function myFunction() {
         var openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
-        }
+        
       }
     }
   }
+}
+
+  const texts=["Nurse","boba fanatic","UX Designer"];
+  let count=0;
+  let index=0;
+  let currentText= "";
+  let letter= "";
+
+  (function type(){
+    if (count === texts.length){
+      count=0;
+    }
+    currentText=texts[count];
+    letter=currentText.slice(0,++index);
+    
+    document.querySelector(".typing").textContent=letter;
+    if (letter.length === currentText.length){
+      count++;
+      index=0;
+    }
+    setTimeout(type,400);
+  })();
